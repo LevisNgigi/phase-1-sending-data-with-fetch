6496e7a25de0de6ec351b945a9816faaa9e9fccd
Levis <Ngigilevis@gmail.com>
2024-01-10 22:30:50 +0300

    practice

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email,) {
+    const url = 'http://localhost:3000/users'
+
+    const data = {
+        name: name,
+        email: email,
+    };
+
+
+return fetch (url, {
+    method: 'POST',
+    headers: {
+        'ContentType': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(data => {
+    console.log('Success', data);
+    return data;
+})
+.catch(error => {
+    console.log('Error', error);
+    return error;
+});
+}
+
+submitData("sivel", "sivel@outlook.com");   
